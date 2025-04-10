6db1c538ffcd9d4e8716876c168a5b587cc01928
Ichikawa75 <101092289+ahiru401066@users.noreply.github.com>
2025-04-10 15:26:00 +0900

    main関数の微修正

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
   fmt.Fprintln(w, "Hello go!")
 }
 
-func main() {
+func main() { 
 	http.HandleFunc("/", helloHandler)
   err := http.ListenAndServe(":8080", nil)
   if err!= nil{
