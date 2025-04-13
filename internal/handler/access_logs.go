954c191c9c0d86ec368edee0e5ae68d4369cedd0
Ichikawa75 <101092289+ahiru401066@users.noreply.github.com>
2025-04-14 00:49:46 +0900

    タブとインデントの修正

--- internal/handler/access_logs.go
+++ internal/handler/access_logs.go
@@ -8,22 +8,23 @@ import (
 )
 
 func AccessLogsHandler(w http.ResponseWriter, r *http.Request) {
-  if r.Method != http.MethodGet {
-    http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-    return
-  }
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+    	return
+	}
 
+	// データベースからアクセスログを取得
 	accessLogs, err := db.ReadAccessLogs()
 	if err != nil {
 		http.Error(w, "Server Error", http.StatusInternalServerError)
 		return		
 	}
 
-  jsonResponse, err := json.Marshal(accessLogs)
-  if err != nil {
+  	jsonResponse, err := json.Marshal(accessLogs)
+	if err != nil {
 		http.Error(w, "Server Error", http.StatusInternalServerError)
-    return    
-  }
+    	return    
+	}
 	w.Header().Set("Content-Type", "application/json")
-  w.Write(jsonResponse)
+	w.Write(jsonResponse)
 }
