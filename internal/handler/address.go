4d9af0fc0981dbe246dacb966b963bc6224a7b1a
Ichikawa75 <101092289+ahiru401066@users.noreply.github.com>
2025-04-13 19:47:28 +0900

    address.goの微修正

--- internal/handler/address.go
+++ internal/handler/address.go
@@ -28,6 +28,7 @@ func isValidPostalCode(postalCode string) bool {
 func AddressHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodGet {
 		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return 
 	}
 
 	// 郵便番号のバリテーション
